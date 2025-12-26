c62d3ac66e80b96930684b4bc95e1388da34d96f
arthi <arthi@MSI>
2025-12-26 17:36:33 +0530

    org test

--- src/test/java/CreateOrgTest.java
+++ src/test/java/CreateOrgTest.java
@@ -1,6 +1,11 @@
+import org.testng.annotations.Test;
 
 public class CreateOrgTest {
 	
 	//hi i made changes in my eclipse 
+	@Test
+	public void orgTest() {
+		System.out.println("---org test----");
+	}
 
 }
