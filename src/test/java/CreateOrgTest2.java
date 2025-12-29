ce2c3e8c8cd99debc0be2944f4d57e4ed6fc5e76
HP <HP@LAPTOP-JMVUU0MR>
2025-12-29 13:04:42 +0530

    i committed one testcase

---
+++ src/test/java/CreateOrgTest2.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class CreateOrgTest2 {
+	
+	//hi i made changes in my eclipse 
+	@Test
+	public void orgTest() {
+		System.out.println("---org test----");
+	}
+
+}
